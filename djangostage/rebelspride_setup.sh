9fff7a004218b23cfb19b61e5a29d560e4a10e49
Daniel Haendel <daniel.haendel@tbs.students.de>
2024-04-11 10:08:15 +0200

    add rebelspride_setup.sh

--- djangostage/rebelspride_setup.sh
+++ djangostage/rebelspride_setup.sh
@@ -15,6 +15,7 @@ echo "
  / _, _/  __/ /_/ /  __/ (__  ) ____/ /  / / /_/ /  __/
 /_/ |_|\___/_.___/\___/_/____/_/   /_/  /_/\__,_/\___/"
 
+sleep 5
 echo "Please wait, updating system..."
 #update system
 sudo apt-get update
